2f10eac39d7c44f944ca96b63e8650951a3e2148
fehmi <fsberin@gmail.com>
2022-01-20 17:21:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("isminizi girin");
+string name = Console.ReadLine();
+Console.WriteLine("soyisminizi girin");
+string surname = Console.ReadLine();
+Console.WriteLine("Merhaba " + name + " " + surname);
+
+
